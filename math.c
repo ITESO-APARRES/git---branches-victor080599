af3ee4fe05889c50659b29fe4c9a93bffd82a604
Victor Prado <victorprado080599@gmail.com>
2022-10-13 14:39:37 -0700

    math.c and math.h

---
+++ math.c
@@ -0,0 +1,8 @@
+// Funciones matematicas para nuestro codigo
+
+int doubleInt(int a){
+    if(a > MAX_INT){
+        return -1;
+    }
+    return a*2;
+}
---
+++ math.h
@@ -0,0 +1,5 @@
+// Encabezados de math
+
+#define MAX_INT (2^sizeof(int))/2
+
+int doubleInt(int a);
